4192958c9c290f1e85414d269b307c3c4e814878
Louay Shaat <shaaloua@amazon.com>
2020-06-22 14:35:29 +1000

    Update index.php

--- src/index.php
+++ src/index.php
@@ -15,9 +15,9 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Telstra CI/CD</h1>
+                <h1>TWE CI/CD</h1>
                 <h2>Congratulations</h2>
-                <p>Your Telstra application is now running on a Docker container in Amazon ECS.</p>
+                <p>Your TWE application is now running on a Docker container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
